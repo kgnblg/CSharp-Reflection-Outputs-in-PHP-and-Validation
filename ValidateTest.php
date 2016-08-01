1d04723080060381ccad7c6279ee1fb869d573bd
kgnblg <kgnblg@gmail.com>
2016-08-01 16:54:49 +0300

    Unit Test

---
+++ ValidateTest.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: KGN
+ * Date: 29.07.2016
+ * Time: 15:35
+ */
+
+namespace Account;
+include_once 'Role.php';
+include_once 'Candidates.php';
+include_once 'Users.php';
+
+class ValidateTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function testGetType()
+    {
+        $testData = "kaganbalga";
+        $tester = new Validate();
+
+        $comingResult = $tester->getType($testData);
+
+        $this->assertEquals("string", $comingResult);
+    }
+
+    public function testValidateVariables()
+    {
+        $users = new Users([
+            'ad' => 'Mehmet',
+            'soyad' => 'OK',
+            'telefonNo' => 12345,
+            'adress' => new Adress(),
+            'role' => [
+                new Role(),
+                new Role(),
+                new Role()
+            ],
+            'test' => [
+                'a' => 1,
+                'b' => 2
+            ],
+            'enum' => 2
+        ]);
+
+        $users->validateVariables();
+
+        $this->expectOutputString('');
+    }
+
+    public function testValidateArrays()
+    {
+        $tester = new Validate();
+        $tester->validateArrays(array(0, 1, 2),"integer");
+
+        $this->expectOutputString('');
+    }
+
+    public function testValidateEnum()
+    {
+        $tester = new Validate();
+        $tester->validateEnum(2, '\Account\Candidates');
+
+        $this->expectOutputString('');
+    }
+}
